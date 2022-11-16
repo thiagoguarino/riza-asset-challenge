a6db32f485bab35e9c4ad63d3978c2386097ebde
Thiago Guarino <thiago.guarino@outlook.com>
2022-11-15 23:47:16 -0300

    refactor: routerLink

--- src/app/components/Header/header.module.ts
+++ src/app/components/Header/header.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from '../Header/header.component';
 
 @NgModule({
   imports: [
+    RouterModule,
     IonicModule
   ],
   declarations: [
