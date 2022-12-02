5e18f9d80490daed6d98a9a4ebba2fa08284a3b4
Thiago Guarino <thiago.guarino@outlook.com>
2022-12-02 00:04:48 -0300

    feat: ArtistDetails Page - done

--- src/app/pages/ArtistDetails/artist-details.module.ts
+++ src/app/pages/ArtistDetails/artist-details.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { ArtistDetailsPage } from './artist-details.page';
 import { HeaderModule } from 'src/app/components/Header/header.module';
 import { ArtistsProfileComponent } from 'src/app/components/ArtistProfile/artistsProfile.component';
-
 import { ArtistDetailsPageRoutingModule } from './artist-details-routing.module';
+import { ArtistButtonModule } from 'src/app/components/ArtistButton/artistButton.module';
 
 @NgModule({
   imports: [
@@ -14,6 +14,7 @@ import { ArtistDetailsPageRoutingModule } from './artist-details-routing.module'
     FormsModule,
     IonicModule,
     HeaderModule,
+    ArtistButtonModule,
     ArtistDetailsPageRoutingModule,
   ],
   declarations: [ArtistDetailsPage, ArtistsProfileComponent]
--- src/app/pages/ArtistDetails/artist-details.page.ts
+++ src/app/pages/ArtistDetails/artist-details.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {Title} from '@angular/platform-browser';
+import { ArtistsInfoService } from '../../services/mousik.services';
 
 @Component({
   selector: 'app-artist-details',
@@ -7,8 +9,22 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./artist-details.scss'],
 })
 
-export class ArtistDetailsPage {
-  constructor(private titleService: Title) {
+export class ArtistDetailsPage implements OnInit {
+  artistParam: string;
+
+  constructor(private titleService: Title, private service: ArtistsInfoService, private route: ActivatedRoute) {
     this.titleService.setTitle('Mousik - Artist Details');
+  }
+
+  ngOnInit(): void {
+    this.route.params
+    .subscribe(params => {
+      this.artistParam = params.name;
+    });
+  }
+
+  async deleteArtist(slug: string): Promise<void> {
+    await this.service.deleteArtist(slug);
+    window.location.reload();
   }
 }
